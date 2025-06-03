d62865893316276aeb495d6b0b0288970f244197
Yasser <yaser01093082389@gmail.com>
2025-06-03 03:43:35 +0300

    added the files to the repo

---
+++ app/companions/page.jsx
@@ -0,0 +1,7 @@
+const CompanionsLibrary = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+const Page = () => {
+  return (
+    <div>
+    <h1 className="text-2x1 underline">Welcome to my SaaS App </h1>
+    <Button>Lets get Started</Button>
+  </div>
+  )
+}
+
+export default Page
